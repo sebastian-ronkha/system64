82530baa80944680cce59fe5c13601e67ad2d6a7
thecodefreak <amaljohnronkha@protonmail.com>
2025-05-24 12:34:18 +0100

    all nice and clean

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/your-repo-name/' : './',
+    base: mode === 'production' ? '/system64/' : './',
   };
 });
